1b32161155043933da0232d6af5f176069033d69
vutsalsinghal <vutsalsinghal@gmail.com>
2018-10-29 13:08:14 -0400

    Updated initial React UI

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+  return (
+    <Menu style={{ marginTop:'0px',}} size={'large'}>
+      <Menu.Item><Link to='/'>Azure UI</Link></Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item><Link to='/someLink'><Icon name='tasks' />Some Link</Link></Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+};
---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {Loader, Dimmer} from 'semantic-ui-react';
+//import {Link} from 'react-router-dom';
+import web3 from '../ethereum/web3';
+
+class Home extends Component {
+    state = {
+    loadingData:false,
+  }
+
+  async componentDidMount(){
+    this.setState({loadingData:true});
+    document.title = "Azure UI";
+    this.setState({loadingData:false});
+  }
+
+
+
+  render() {
+    if(this.state.loadingData){
+      return (
+        <Dimmer active inverted>
+        <Loader size='massive'>Loading...</Loader>
+        </Dimmer>
+      );
+    }
+
+    return (
+      <div>
+        <h1>Hi There!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;

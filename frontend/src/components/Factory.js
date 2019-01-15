ceb15b9c4fc186c23047bb65bf237d5b837556bc
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-01-15 22:53:28 +0530

    added basic pagination

--- frontend/src/components/Factory.js
+++ frontend/src/components/Factory.js
@@ -15,7 +15,10 @@ class Factory extends Component {
     freightCarrierAddress: '',
     originCustomsAddress: '',
     consigneeAddress: '',
+    deployedChainsAddr: [],
     deployedChains: [],
+    currentPage: 1,
+    chainsPerPage: 2,
   }
 
   async componentDidMount() {
@@ -23,17 +26,50 @@ class Factory extends Component {
     document.title = "Azure UI";
 
     const accounts = await web3.eth.getAccounts();
-    let deployedChains = await FactoryInstance.methods.getDeployedSupplyChain().call({ from: accounts[0] });
-
-    let arr = [];
-    for (var i = 0; i < deployedChains.length; i++) {
-      const SupplyChainInstance = await supplychain_instance(deployedChains[i]);
-      let contractDesc = await SupplyChainInstance.methods.Description().call({ from: accounts[0] });
-      let contractState = await SupplyChainInstance.methods.State().call({ from: accounts[0] });
-      arr.push([deployedChains[i], contractDesc, contractState]);
+    let deployedChainsAddr = await FactoryInstance.methods.getDeployedSupplyChain().call({ from: accounts[0] });
+    
+    let last = this.state.chainsPerPage;
+    if (deployedChainsAddr.length < last){
+      last = deployedChainsAddr.lenght;
     }
 
-    this.setState({ loadingData: false, account: accounts[0], deployedChains: arr });
+    let deployedChains = [];
+    for (var i = 0; i < last; i++) {
+      const SupplyChainInstance = await supplychain_instance(deployedChainsAddr[i]);
+      let contractDesc = await SupplyChainInstance.methods.Description().call({ from: accounts[0] });
+      let contractState = await SupplyChainInstance.methods.State().call({ from: accounts[0] });
+      deployedChains.push([deployedChainsAddr[i], contractDesc, contractState]);
+    }
+
+    this.setState({ loadingData: false, account: accounts[0], deployedChainsAddr, deployedChains });
+  }
+
+  handleClick = (event) => {
+    const currentPage = Number(event.target.id);
+    const indexOfLastChain = currentPage * this.state.chainsPerPage;
+    const indexOfFirstChain = indexOfLastChain - this.state.chainsPerPage;
+    
+    this.getChains(indexOfFirstChain, indexOfLastChain);
+    this.setState({ currentPage });
+  }
+
+  getChains = async (first, last) => {
+    this.setState({ loadingData: true });
+    const { account, deployedChainsAddr } = this.state;
+
+    if (last > this.state.deployedChainsAddr.length){
+      last = this.state.deployedChainsAddr.length;
+    }
+
+    let deployedChains = [];
+    for (var i = first; i < last; i++) {
+      const SupplyChainInstance = await supplychain_instance(deployedChainsAddr[i]);
+      let contractDesc = await SupplyChainInstance.methods.Description().call({ from: account });
+      let contractState = await SupplyChainInstance.methods.State().call({ from: account });
+      deployedChains.push([deployedChainsAddr[i], contractDesc, contractState]);
+    }
+
+    this.setState({ deployedChains, loadingData: false });
   }
 
   renderChains = () => {
@@ -85,44 +121,62 @@ class Factory extends Component {
       statusMessage = <Message floating positive header="Success!" content={this.state.msg} />;
     }
 
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(this.state.deployedChainsAddr.length / this.state.chainsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <Button key={number} id={number} onClick={this.handleClick}>{number}</Button>
+      );
+    });
+
     return (
       <div>
         <h1>Deployed Supplychain Transportation Contracts</h1>
         <Grid stackable reversed='mobile'>
-          <Grid.Column width={12}>
-            {this.state.deployedChains.length > 0 && this.renderChains()}
-            {this.state.deployedChains.length === 0 && <p>No contracts deployed!</p>}
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Grid.Row>
-              <Modal trigger={<Button primary icon labelPosition='right'><Icon name='plus circle' />Deploy New Supplychain</Button>}>
-                <Modal.Header>Supplychain Transportation Factory</Modal.Header>
-                <Modal.Content>
-                  <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
-                    <Form.Field>
-                      <label>Description</label>
-                      <Input onChange={event => this.setState({ description: event.target.value })} />
-                    </Form.Field>
-                    <Form.Field>
-                      <label>Freight Carrier Address</label>
-                      <Input onChange={event => this.setState({ freightCarrierAddress: event.target.value })} />
-                    </Form.Field>
-                    <Form.Field>
-                      <label>Oigin Customs Address</label>
-                      <Input onChange={event => this.setState({ originCustomsAddress: event.target.value })} />
-                    </Form.Field>
-                    <Form.Field>
-                      <label>Consignee Address</label>
-                      <Input onChange={event => this.setState({ consigneeAddress: event.target.value })} />
-                    </Form.Field>
-                    <Button loading={this.state.loading} disabled={this.state.loading} primary basic type='submit'>Deploy</Button>
-                    <Message error header="Oops!" content={this.state.errorMessage} />
-                    {statusMessage}
-                  </Form>
-                </Modal.Content>
-              </Modal>
-            </Grid.Row>
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column width={12}>
+              {this.state.deployedChainsAddr.length > 0 && this.renderChains()}
+              {this.state.deployedChainsAddr.length === 0 && <p>No contracts deployed!</p>}
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Grid.Row>
+                <Modal trigger={<Button primary icon labelPosition='right'><Icon name='plus circle' />Deploy New Supplychain</Button>}>
+                  <Modal.Header>Supplychain Transportation Factory</Modal.Header>
+                  <Modal.Content>
+                    <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+                      <Form.Field>
+                        <label>Description</label>
+                        <Input onChange={event => this.setState({ description: event.target.value })} />
+                      </Form.Field>
+                      <Form.Field>
+                        <label>Freight Carrier Address</label>
+                        <Input onChange={event => this.setState({ freightCarrierAddress: event.target.value })} />
+                      </Form.Field>
+                      <Form.Field>
+                        <label>Oigin Customs Address</label>
+                        <Input onChange={event => this.setState({ originCustomsAddress: event.target.value })} />
+                      </Form.Field>
+                      <Form.Field>
+                        <label>Consignee Address</label>
+                        <Input onChange={event => this.setState({ consigneeAddress: event.target.value })} />
+                      </Form.Field>
+                      <Button loading={this.state.loading} disabled={this.state.loading} primary basic type='submit'>Deploy</Button>
+                      <Message error header="Oops!" content={this.state.errorMessage} />
+                      {statusMessage}
+                    </Form>
+                  </Modal.Content>
+                </Modal>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row centered>
+            <Button.Group>
+              {renderPageNumbers}
+            </Button.Group>
+          </Grid.Row>
         </Grid>
       </div>
     );

7e7f8aaaf6c4fe25472c74bcd7a36a83c816f44a
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-06-02 22:22:04 -0400

    updated build/index.html

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/UI-project/precache-manifest.d9d363f5b650e276833458e46f6a5583.js"
+  "/UI-project/precache-manifest.099ae54931178fca7329bc01539dda25.js"
 );
 
 workbox.clientsClaim();
